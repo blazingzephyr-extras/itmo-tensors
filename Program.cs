ab7e4e15e75742da15cf799b8b371c1df500b507
blazingzephyr <blazingzephyr@proton.me>
2026-05-01 02:55:05 +0300

    Добавить тензорное произведение (outer product)

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿
+using System.Numerics.Tensors;
+
+namespace blazingzephyr.itmo.tensors;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Примитивные тензорные / SIMD операции из .NET8:");
+        Console.ReadKey();
+        Example1();
+
+        Console.WriteLine("----------");
+        Console.ReadKey();
+        Example2();
+
+        Console.WriteLine();
+        Console.WriteLine("Собственная имплементация тензоров:");
+        Console.ReadKey();
+        OuterProduct1();
+
+        Console.WriteLine();
+        Console.WriteLine("Собственная имплементация тензоров:");
+        Console.ReadKey();
+        OuterProduct2();
+    }
+
+    static void Example1()
+    {
+        // Встроенная имплементация из .NET 8 (ноябрь 2023)
+        // Примитивные операции с тензорами.
+        //
+        ReadOnlySpan<double> x = [10.0, 5.0, 6.0, 8.0, 9.0];
+        Console.WriteLine(String.Join(", ", x.ToArray()));
+
+        double y = 8.0;
+        Console.WriteLine($" + {y}");
+
+        Span<double> result = stackalloc double[5];
+        System.Numerics.Tensors.TensorPrimitives.Add(x, y, result);
+
+        Console.WriteLine();
+        Console.WriteLine(String.Join(", ", result.ToArray()));
+    }
+
+    static void Example2()
+    {
+        // Встроенная имплементация из .NET 8 (ноябрь 2023)
+        // Примитивные операции с тензорами.
+        //
+        ReadOnlySpan<System.Numerics.Complex> x = [
+            new System.Numerics.Complex(11.0, 4.0),
+            new System.Numerics.Complex(2.0, 4.0),
+            new System.Numerics.Complex(5.0, 0.0)
+        ];
+        Console.WriteLine(String.Join(", ", x.ToArray()));
+        Console.WriteLine("Проверка Span<T> на комплексность.");
+
+        Span<bool> result = stackalloc bool[3];
+        System.Numerics.Tensors.TensorPrimitives.IsComplexNumber(x, result);
+
+        Console.WriteLine();
+        Console.WriteLine(String.Join(", ", result.ToArray()));
+    }
+
+    static void OuterProduct1()
+    {
+        Span<double> a = [1, 0, 0, 1];
+        Tensor<double> A = new Tensor<double>(a, 2, 2);
+
+        Span<double> b = [1, 1, 1, -1];
+        Tensor<double> B = new Tensor<double>(b, 2, 2);
+
+        Span<double> c = stackalloc double[2 * 2 * 2 * 2];
+        Tensor<double> C = Tensor<double>.OuterProduct(A, B, c);
+
+        Console.WriteLine(C[0, 0, 1, 1]);
+    }
+
+    static void OuterProduct2()
+    {
+        Span<double> a = [1, 2, 3, 4];
+        Tensor<double> A = new Tensor<double>(a, 4, 1);
+
+        Span<double> b = [5, 6, 7, 8];
+        Tensor<double> B = new Tensor<double>(b, 1, 4);
+
+        Span<double> c = stackalloc double[2 * 2 * 2 * 2];
+        Tensor<double> C = Tensor<double>.OuterProduct(A, B, c);
+
+        Console.WriteLine(A[2, 0]);
+        Console.WriteLine(B[0, 3]);
+        Console.WriteLine(C[2, 0, 0, 2]);
+    }
+}
---
+++ Tensor.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Numerics;
+
+namespace blazingzephyr.itmo.tensors;
+
+public ref struct Tensor<T> where T : unmanaged, INumberBase<T>
+{
+    private readonly int[] _shape;
+    private readonly int[] _strides;
+    private Span<T> _data;
+
+    public Tensor(Span<T> data, params int[] shape)
+    {
+        _data = data;
+        _shape = shape;
+        _strides = CalculateStrides(shape);
+    }
+
+    public ref T this[ReadOnlySpan<int> indices] => ref _data[FlatIndex(indices)];
+    public ref T this[params int[] indices] => ref _data[FlatIndex(indices)];
+
+    private Tensor(T[] data, int[] shape, int[] strides)
+    {
+        _data = data;
+        _shape = shape;
+        _strides = strides;
+    }
+
+    private static int[] CalculateStrides(int[] shape)
+    {
+        int[] strides = new int[shape.Length];
+        strides[^1] = 1;
+        for (int i = shape.Length - 2; i >= 0; i--)
+            strides[i] = strides[i + 1] * shape[i + 1];
+
+        return strides;
+    }
+
+    public readonly int FlatIndex(ReadOnlySpan<int> indices)
+    {
+        int result = 0;
+        for (int i = 0; i < indices.Length; i++)
+        {
+            result += indices[i] * _strides[i];
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Тензорное произведение тензоров.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="dest"></param>
+    public static Tensor<T> OuterProduct(Tensor<T> a, Tensor<T> b, Span<T> dest)
+    {
+        int[] shape = [.. a._shape, .. b._shape];
+        Tensor<T> result = new Tensor<T>(dest, shape);
+        Span<int> ai = stackalloc int[a._shape.Length];
+        Span<int> bi = stackalloc int[b._shape.Length];
+        for (int flat = 0; flat < dest.Length; flat++)
+        {
+            int i = flat;
+            for (int j = 0; j < a._shape.Length; j++)
+            {
+                ai[j] = i / result._strides[j];
+                i %= result._strides[j];
+            }
+            for (int j = 0; j < b._shape.Length; j++)
+            {
+                bi[j] = i / result._strides[j + a._shape.Length];
+                i %= result._strides[j + a._shape.Length];
+            }
+            dest[flat] = a._data[a.FlatIndex(ai)] * b._data[b.FlatIndex(bi)];
+        }
+        return result;
+    }
+}
